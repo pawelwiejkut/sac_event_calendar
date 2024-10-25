db6a797e4fa389c3eae01a8f0c8294ad3b62560f
Paweł Wiejkut <poczta@pawelwiejkut.net>
2024-10-25 20:29:02 +0200

    Create test_events.js

---
+++ test_events.js
@@ -0,0 +1,95 @@
+this.events = [
+          {
+            startDate: "2024-10-01T15:00:00",
+            endDate: "2024-10-04T12:00:00",
+            event: "Birthday Party",
+          },
+          {
+            startDate: "2024-10-01T10:00:00",
+            endDate: "2024-10-04T18:00:00",
+            event: "Work",
+          },
+          {
+            startDate: "2024-10-02T15:00:00",
+            endDate: "2024-10-05T12:00:00",
+            event: "One position",
+          },
+          {
+            startDate: "2024-10-02T23:59:00",
+            endDate: "2024-10-04T00:00:00",
+            event: "Test 3",
+          },
+
+          {
+            startDate: "2024-10-16T01:59:00",
+            endDate: "2024-10-17T06:00:00",
+            event: "Test 3",
+          },
+
+          {
+            startDate: "2024-10-12T14:00:00",
+            endDate: "2024-10-13T16:00:00",
+            event: "Meeting",
+          },
+
+          {
+            startDate: "2024-10-19T10:35:00",
+            endDate: "2024-10-22T16:00:00",
+            event: "Semi-Week",
+          },
+          {
+            startDate: "2024-10-25T00:00:00",
+            endDate: "2024-10-25T16:00:00",
+            event: "One Day",
+          },
+          {
+            startDate: "2024-10-26T09:00:00",
+            endDate: "2024-10-26T22:00:00",
+            event: "One Day",
+          },
+
+          {
+            startDate: "2024-10-30T09:00:00",
+            endDate: "2024-11-26T22:00:00",
+            event: "One Day",
+          },
+
+          {
+            startDate: "2024-10-07T01:00:00",
+            endDate: "2024-10-08T23:59:00",
+            event: "Hour test",
+          },
+
+          {
+            startDate: "2024-10-06T11:00:00",
+            endDate: "2024-10-11T23:59:00",
+            event: "Hour test",
+          },
+
+          {
+            startDate: "2024-10-03T01:00:00",
+            endDate: "2024-10-05T23:59:00",
+            event: "Hour test",
+          },
+
+          {
+            startDate: "2024-10-05T01:00:00",
+            endDate: "2024-10-06T23:59:00",
+            event: "Hour test",
+          },
+          {
+            startDate: "2024-10-05T01:00:00",
+            endDate: "2024-10-06T23:59:00",
+            event: "Hour test",
+          },
+          {
+            startDate: "2024-10-05T01:00:00",
+            endDate: "2024-10-06T23:59:00",
+            event: "Hour test",
+          },
+          {
+            startDate: "2024-10-05T01:00:00",
+            endDate: "2024-10-06T23:59:00",
+            event: "Hour test",
+          },
+        ];
